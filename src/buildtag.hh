b305077bfbe819c19847bfc96f63b4c842c05a57
j_sauermann <j_sauermann@bd74f7bd-1a55-4bac-9fab-68015b139e80>
2014-01-13 18:45:18 +0000

    tagging Release 1.2
    git-svn-id: http://svn.savannah.gnu.org/svn/apl/tags/apl-1.2@94 bd74f7bd-1a55-4bac-9fab-68015b139e80

--- src/buildtag.hh
+++ src/buildtag.hh
@@ -1,2 +1,2 @@
 #include "Common.hh"
-#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 5212", "2014-01-08 18:43:25 UTC", "Linux 2.6.32-54-generic i686", ""
+#define BUILDTAG PACKAGE_NAME, PACKAGE_VERSION " / 5278", "2014-01-13 18:39:29 UTC", "Linux 2.6.32-54-generic i686", ""
